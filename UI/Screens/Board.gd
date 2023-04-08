5ff3b1444fe2f7d45bdfcf99e4bc6a043a4d691a
Skeif_ <victorskos@gmail.com>
2023-04-08 21:15:37 +0700

    Added an animation at every move

--- UI/Screens/Board.gd
+++ UI/Screens/Board.gd
@@ -3,7 +3,8 @@ extends ScrollContainer
 onready var BoardFields = $Background
 
 var PlayersTimer
-var _moving_players = []
+# Первый элемент это актуальная позиция, второй элемент это позиция куда надо следовать.
+var _moving_players = {}
 
 func _ready():
 	#var BoardPath = Utility.read_json_file("user://BoardPath.json")
@@ -28,18 +29,31 @@ func set_player_to_board(player, position):
 	#		return
 	
 	#_moving_players.append({player : position})
-	if(player.get_parent() != null):
-		player.get_parent().remove_child(player)
 	
-	var Place = BoardFields.get_child(position).get_node("PlacePlayers")
-	if(Place.get_child(0).get_child_count() < 2):
-		Place.get_child(0).add_child(player)
+	if(not player in _moving_players):
+		_moving_players[player] = [position-1, position]
 	else:
-		Place.get_child(1).add_child(player)
+		_moving_players[player][1] = position
 
 func get_field(position):
 	return BoardFields.get_child(position)
 
 func _update_players_boards():
 	for player in _moving_players:
-		pass
+		if(_moving_players[player][0] != _moving_players[player][1]):
+			if _moving_players[player][1] < _moving_players[player][0]:
+				_moving_players[player][0] -= 1
+			else:
+				_moving_players[player][0] += 1
+		else:
+			continue
+		
+		if(player.get_parent() != null):
+			player.get_parent().remove_child(player)
+		
+		var Place = BoardFields.get_child(_moving_players[player][0]).get_node("PlacePlayers")
+		if(Place.get_child(0).get_child_count() < 2):
+			Place.get_child(0).add_child(player)
+		else:
+			Place.get_child(1).add_child(player)
+			
